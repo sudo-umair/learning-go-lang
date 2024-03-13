bf2ae3d0c10f43a83e9e60433fc88a77992c4c7f
Muhammad Umair <muhammadumair523@gmail.com>
2024-03-13 16:30:49 +0500

    infer type in go lang

--- module-02/investment-calculator.go
+++ module-02/investment-calculator.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
+	expectedReturnRate := 5.5
 	var years float64 = 5
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println("Future value of investment:", futureValue)
 }
