aa864159fda2ac3bdd3f70f68b2102fcb807799d
Muhammad Umair <muhammadumair523@gmail.com>
2024-03-13 16:35:12 +0500

    constants in go lang

--- module-02/investment-calculator.go
+++ module-02/investment-calculator.go
@@ -6,10 +6,13 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount, years := 1000.0, 5.0
 	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("Future value of investment:", futureValue)
+	fmt.Println("Future real value of investment:", futureRealValue)
 }
