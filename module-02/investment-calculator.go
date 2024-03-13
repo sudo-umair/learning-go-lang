20293f9c2d4e396347bff35285f7a3f10535cd10
Muhammad Umair <muhammadumair523@gmail.com>
2024-03-13 16:29:20 +0500

    explicit type assertion

--- module-02/investment-calculator.go
+++ module-02/investment-calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 5
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println("Future value of investment:", futureValue)
 }
