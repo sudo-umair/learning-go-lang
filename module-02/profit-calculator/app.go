a5dd6a5ed8cff92d53f0f348775740e9ac3075f0
Muhammad Umair <muhammadumair523@gmail.com>
2024-03-14 22:52:00 +0500

    functions exercise

--- module-02/profit-calculator/app.go
+++ module-02/profit-calculator/app.go
@@ -3,25 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := getInputFor("Enter revenue: ")
+	expenses := getInputFor("Enter expenses: ")
+	taxRate := getInputFor("Enter tax rate: ")
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	ebt, profit, ratio := calculateProfit(revenue, expenses, taxRate)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	fmt.Printf("Earnings before tax: %.2f\n", ebt)
+	fmt.Printf("Profit: %.2f\n", profit)
+	fmt.Printf("Earnings before tax to profit ratio: %.2f\n", ratio)
+}
 
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
+func getInputFor(text string) float64 {
+	var value float64
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
+}
 
+func calculateProfit(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
-
-	fmt.Println("Earnings before tax:", ebt)
-	fmt.Println("Profit:", profit)
-	fmt.Println("Earnings before tax to profit ratio:", ratio)
-
+	return ebt, profit, ratio
 }
